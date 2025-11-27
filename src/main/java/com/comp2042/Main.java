f0a2e1de87a3365390f6b7269dde1b96632b0eab
mortalzathen <athulpraveen07@outlook.com>
2025-11-28 02:45:35 +0800

    updating css and the ghost block. also added a leaderboard

--- src/main/java/com/comp2042/Main.java
+++ src/main/java/com/comp2042/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,8 +14,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         URL location = getClass().getClassLoader().getResource("menuLayout.fxml");
+        Font.loadFont(getClass().getClassLoader().getResource("digital.ttf").toExternalForm(), 38);
         ResourceBundle resources = null;
         FXMLLoader fxmlLoader = new FXMLLoader(location, resources);
         Parent root = fxmlLoader.load();
